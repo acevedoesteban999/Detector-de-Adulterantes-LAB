a62976b038c9de9132e229583991eb4a100c098c
Esteban <acevedoesteban999@gmail.com>
2023-07-25 00:36:06 +0800

    gitignore, groups and permissions

--- README.md
+++
@@ -1,3 +0,0 @@
-## Djagno-Adminlte Windows Development
-[comment]: <> (Version main-footer)
-> Rama para desarrollo del proyecto
--- core/log/utils.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-# Create your views here.
-from django.urls import reverse_lazy
-
-class MyLoginRequiredMixin(LoginRequiredMixin):
-    login_url = reverse_lazy('login')
