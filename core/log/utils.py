206c5f59f227bee75376e73fc49fd67fae8a802e
Esteban <acevedoesteban999@gmail.com>
2023-07-28 16:40:05 +0800

    Charts

--- core/log/utils.py
+++ core/log/utils.py
@@ -8,6 +8,9 @@ class MyLoginRequiredMixin(LoginRequiredMixin,PermissionRequiredMixin):
     permission_required=None
     permission_denied_message="Permisos insuficientes para acceder a esta zona"
     
+    def is_ajax(self):
+        return self.request.headers.get('x-requested-with') == 'XMLHttpRequest'
+    
     def has_permission(self) -> bool:
         if self.permission_required==None:
             return True
--- core/process/views.py
+++ core/process/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from core.log.utils import MyLoginRequiredMixin
+from django.http import JsonResponse  
+import random
 # Create your views here.
 
 
 class ProcessView(MyLoginRequiredMixin,TemplateView):
     template_name='process.html'
+    def post(self,request):
+        if self.is_ajax():
+            if request.POST.get('action')=="data":
+                return JsonResponse({'response':random.randint(0,25)},safe=False)
+        return None
+        
