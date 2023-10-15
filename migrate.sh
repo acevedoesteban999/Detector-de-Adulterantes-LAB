c72aacfa9d6513a4ea51e4e2a54a5e6425fe5bb0
Esteban Acevedo <acevedoesteban999@gmail.com>
2023-10-15 17:25:46 +0100

    .

--- migrate.sh
+++ migrate.sh
@@ -3,7 +3,6 @@ sudo -s source .venv/bin/activate;
 python manage.py makemigrations;
 python manage.py migrate;
 yes yes | python manage.py collectstatic;
-#chmod 777 update.sh
 sudo chmod 777 ./db.sqlite3
 sudo chmod 777 ./
 sudo chown :www-data ./db.sqlite3
