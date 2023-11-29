2d4d9797c259fdba9de51fa28b2cb07438790c48
Esteban <acevedoesteban999@gmail.com>
2023-11-28 23:19:52 -0500

    predict model field

--- core/pred/models.py
+++ core/pred/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from core.meas.models import Measuring
 from config.utils import PredictionChoices 
+from core.mod.models import Model
 # Create your models here.
 
 
@@ -8,6 +9,7 @@ class Prediction(models.Model):
     name=models.CharField(verbose_name="Nombre",unique=True,max_length=15)
     state=models.BooleanField(default=None,null=True)
     datetime=models.DateTimeField(auto_now_add=True)
+    model=models.ForeignKey(Model, on_delete=models.CASCADE)
     def __str__(self):
         return  self.name
 
